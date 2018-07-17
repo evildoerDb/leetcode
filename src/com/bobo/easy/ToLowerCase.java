79e52dbb65492b39c08a3a536a0a50a96550734c
bobo.dou <bobo.dou@weimob.com>
2018-07-17 17:22:16 +0800

    toLowerCase

---
+++ src/com/bobo/easy/ToLowerCase.java
@@ -0,0 +1,24 @@
+package com.bobo.easy;
+
+/**
+ * Created by bobo.dou@weimob.com on 2018/7/17 17:19
+ *
+ * 将字符串转换成小写
+ * Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+ */
+public class ToLowerCase {
+
+    /**
+     * 不要脸的方法
+     * @param str
+     * @return
+     */
+    public static String toLowerCase(String str) {
+
+        return str.toLowerCase();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
