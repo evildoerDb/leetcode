055c3ff6838d83533731da859d971dfd8467e93e
bobo.dou <bobo.dou@weimob.com>
2018-07-17 16:40:10 +0800

    初始化

---
+++ src/com/bobo/easy/Main.java
@@ -0,0 +1,11 @@
+package com.bobo.easy;
+
+/**
+ * Created by bobo.dou@weimob.com on 2018/7/17 16:33
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("haha");
+    }
+}
