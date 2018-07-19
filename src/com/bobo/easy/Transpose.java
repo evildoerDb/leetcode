d2190128d84c46f1bfaa411e0f82433700dce63e
evildoerDb <179453684@qq.com>
2018-07-20 00:18:09 +0800

    peakIndexInMountainArray

---
+++ src/com/bobo/easy/Transpose.java
@@ -0,0 +1,25 @@
+package com.bobo.easy;
+
+/**
+ * Created by evildoerdb_ on 2018/7/19
+ *
+ * Given a matrix A, return the transpose of A.
+ *
+ * The transpose of a matrix is the matrix
+ * flipped over it's main diagonal, switching the row and column indices of the matrix.
+ *
+ *
+ * 867
+ */
+public class Transpose {
+
+    public int[][] transpose(int[][] A) {
+        int row = 0;
+        int col = 0;
+        while (row < A[0].length){
+            //TODO
+        }
+
+        return A;
+    }
+}
