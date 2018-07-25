8aea4799a31c6c1e6722b004210b4347e24f7e4e
bobo.dou <bobo.dou@weimob.com>
2018-07-25 17:40:23 +0800

    up

---
+++ src/com/bobo/easy/FindWords.java
@@ -0,0 +1,26 @@
+package com.bobo.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by evildoerDb on 2018/7/25 16:57
+ *
+ * Given a List of words, return the words that can be typed
+ * using letters of alphabet on only one row's of American keyboard like the image below.
+ *
+ * Input: ["Hello", "Alaska", "Dad", "Peace"]
+ * Output: ["Alaska", "Dad"]
+ *
+ * 500
+ */
+public class FindWords {
+
+    //TODO
+    //待完善
+    public String[] findWords(String[] words) {
+
+        // return Stream.of(words).filter(s -> s.toLowerCase().matches("[qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*")).toArray(String[]::new);
+        return null;
+    }
+}
---
+++ src/com/bobo/easy/IsToeplitzMatrix.java
@@ -0,0 +1,19 @@
+package com.bobo.easy;
+
+/**
+ * Created by evildoerDb on 2018/7/25 17:29
+ *
+ *  matrix is Toeplitz if every diagonal from top-left to bottom-right has the same element.
+ *
+ * Now given an M x N matrix, return True if and only if the matrix is Toeplitz.
+ *
+ * 766
+ */
+public class IsToeplitzMatrix {
+
+    public boolean isToeplitzMatrix(int[][] matrix) {
+
+        //TODO
+        return false;
+    }
+}
