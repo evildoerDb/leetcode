aa8cde2b3e3f476569613b58712875158d11e547
bobo.dou <bobo.dou@weimob.com>
2018-07-31 18:09:51 +0800

    up

---
+++ src/com/bobo/easy/LongestCommonPrefix.java
@@ -0,0 +1,32 @@
+package com.bobo.easy;
+
+/**
+ * Created by evildoerDb on 2018/7/31 18:04
+ *
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ *
+ * If there is no common prefix, return an empty string "".
+ *
+ * Example 1:
+ *
+ * Input: ["flower","flow","flight"]
+ * Output: "fl"
+ * Example 2:
+ *
+ * Input: ["dog","racecar","car"]
+ * Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ * Note:
+ *
+ * All given inputs are in lowercase letters a-z.
+ *
+ *
+ * 14
+ */
+public class LongestCommonPrefix {
+
+    public String longestCommonPrefix(String[] strs) {
+        //TODO
+        return "";
+    }
+}
